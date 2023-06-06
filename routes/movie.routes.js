0731d5250d9ea5d115c382730acbd571935296e1
Toni Soler <tonisoler1986@gmail.com>
2023-06-06 18:44:56 +0200

    route details working here

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const axios = require('axios');
 
 
-router.get('/:movieId', (req, res, next) => {
-    const { movieId } = req.params;
+
+// router.get('/:movieId', (req, res, next) => {
+//     const { movieId } = req.params;
   
-    // Make a request to the IMDb API to get movie details
-    axios.get(`https://imdb-api.com/en/API/Title/k_xmndj5an/${movieId}`)
-      .then((response) => {
-        const movieDetails = response.data;
-        res.json(movieDetails);
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(500).json({ error: 'Failed to retrieve movie details' });
-      });
-  });
+//     // Make a request to the IMDb API to get movie details
+//     axios.get(`https://imdb-api.com/en/API/Title/k_xmndj5an/${movieId}`)
+//       .then((response) => {
+//         const movieDetails = response.data;
+//         res.json(movieDetails);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//         res.status(500).json({ error: 'Failed to retrieve movie details' });
+//       });
+//   });
   
   module.exports = router;
 
